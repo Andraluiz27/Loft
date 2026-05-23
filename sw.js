8f781ede8574a6465c9b224e7cfd2ab5d6d97874
Andraluiz27 <108290365+Andraluiz27@users.noreply.github.com>
2026-05-22 21:13:35 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'loft-v1';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
